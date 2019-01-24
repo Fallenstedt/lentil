42bfa2e14631689495c5dc62315d155ca5c7c6fe
Alex Fallenstedt <a.fallenstedt@gmail.com>
2019-01-23 20:09:12 -0800

    print options to screen

--- example/main.dart
+++ example/main.dart
@@ -1,6 +1,15 @@
 import "package:lentil/src/terminal.dart";
+import "package:lentil/src/option.dart";
 
 void main() {
   var terminal = new Terminal();
-  terminal.printPrompt('Hello World!');
+
+  var options = [
+    new Option('I want red', '#f00'),
+    new Option('I want blue', '#00f'),
+  ];
+  terminal.clearScreen();
+  terminal.printOptions(options);
+  var response = terminal.collectInput();
+  print(response);
 }
--- lib/src/terminal.dart
+++ lib/src/terminal.dart
@@ -1,7 +1,29 @@
 import 'dart:io';
+import 'option.dart';
 
 class Terminal {
   void printPrompt(String prompt) {
     stdout.writeln(prompt);
+  }
+
+  void printOptions(List<Option> options) {
+    for (var i = 0; i < options.length; i++) {
+      stdout.writeln('[$i] - ${options[i].label}');
+    }
+    stdout.writeln('Enter a choice \n');
+    stdout.write('>');
+  }
+
+  String collectInput() {
+    return stdin.readLineSync();
+  }
+
+  void clearScreen() {
+    if (Platform.isWindows) {
+      // https://en.wikipedia.org/wiki/ANSI_escape_code
+      stdout.write('\x1B[2J\x1b[0f');
+    } else {
+      stdout.write('\x1B[2J\x1B[3BJ\x1B[H');
+    }
   }
 }
